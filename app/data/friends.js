5b89b9421e3e71688164cb30c3203d754069621a
Pickle Rick <dmryan711@gmail.com>
2019-04-15 01:18:14 -0400

    Modularized routing

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+
+var friends = [{
+    "name":"Devon",
+    "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiClNPNq9HhAhVpUd8KHddPDtMQjRx6BAgBEAU&url=https%3A%2F%2Fftw.usatoday.com%2F2017%2F01%2Frandy-moss-20-points-per-game-nba-kevin-garnett-latrell-sprewell-basketball&psig=AOvVaw3C-zqf0GIuSxBrmqgAPlB-&ust=1555391659584125",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }];
+  
