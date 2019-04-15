40534ede793dbb531dbc9077d3931226fd61b335
Pickle Rick <dmryan711@gmail.com>
2019-04-15 13:27:01 -0400

    Friends are being compared and saving to file

--- server.js
+++ server.js
@@ -4,22 +4,14 @@ const   express = require("./node_modules/express"),
 const app =  express();
 const path = require("path");
 
-
-
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use('/',htmlRoutes);
 app.use('/api/friends',apiRoutes);
 
-
 const PORT = process.env.PORT || 8080;
 
-
-
-
-
 // START THE SERVER
-// =============================================================================
 app.listen(PORT, function(){
     console.log("Listening on port "+PORT);
 });
